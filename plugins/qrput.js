be0d71ba9082a9f2f8287e616ab489ee3fb8488d
Putbotz <96361236+Putbotz@users.noreply.github.com>
2024-05-02 22:02:38 +0800

    Update qrput.js

--- plugins/qrput.js
+++ plugins/qrput.js
@@ -1,7 +1,7 @@
 let fetch = require('node-fetch')
 
 let handler = async (m, { conn, command }) => {
-    let buffer = await fetch(https://telegra.ph/file/896db9d9bb6086d47dbd9.jpg).then(res => res.buffer())
+    let buffer = await fetch(https://telegra.ph/file/10d188d36dcb48999ae0f.jpg).then(res => res.buffer())
     conn.sendFile(m.chat, buffer, 'putxd.jpg', *SILAKAN DISCAN YA BOSSKU.*, m)
 }
 
