75452b1d21be03d9497fe141f19e5ad7dbad87b5
Putbotz <96361236+Putbotz@users.noreply.github.com>
2024-08-23 15:58:21 +0800

    Update config.js

--- config.js
+++ config.js
@@ -26,7 +26,7 @@ global.packname = 'Made by'
 global.author = 'Putbotz'
 global.alpiskey = 'YOUR_APIKEY_HERE' //daftar sendiri jika key habis https://alpis.eu.org
 
-global.btc = 'YOUR_APIKEY_HERE' //Daftar terlebih dahulu https://api.botcahx.live
+global.btc = 'putxd' //Daftar terlebih dahulu https://api.botcahx.live
 global.lann = 'YOUR_APIKEY_HERE' //Note Key Ini Hanya Bertahan Selama 4 Day Sejak Config.js update! lebih baik register di https://api.betabotz.me
 global.APIs = { 
   tio: 'https://api.botcahx.live',
