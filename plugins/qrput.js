c740715969e4813049cc896ffd99dd705741f2e9
Putbotz <96361236+Putbotz@users.noreply.github.com>
2025-02-18 21:06:03 +0800

    Update qrput.js

--- plugins/qrput.js
+++ plugins/qrput.js
@@ -1,7 +1,7 @@
 let fs = require('fs')
 
 let handler = async (m, { conn, command }) => {
-    let buffer = fs.readFileSync('img/put.png')  // Reading the local file
+    let buffer = fs.readFileSync('img/IMG-20250130-WA0021.jpg')  // Reading the local file
     conn.sendFile(m.chat, buffer, 'putxd.png', '*SILAKAN DISCAN YA BOSSKU.*', m)
 }
 
